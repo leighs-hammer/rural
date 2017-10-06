b633ffd1fbfc6c56df80a765fc675c6475a3f25f
Leigh Barnes <leighbrendonbarnes@gmail.com>
2017-10-07 00:50:29 +0100

    Outline for index.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /**
  * Countries
  */
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
+// default
+export default function(code,options,debug) {
+	// Options Modifiers
+
+	// Remove on production
+	// Logs out the params if the debug is passed
+	if(debug !== undefined && debug === true) {
+		console.log(code);
+		console.log(options);
+	}
+	// Return
+
 }
+
+// Ancsiliraries
+
+export function ancs(){
+  console.log('ancilirary');
+}
+export { ancs as ruralRaw };
